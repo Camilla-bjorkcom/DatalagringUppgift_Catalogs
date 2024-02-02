ca9b56f92a38ffb5eda23d1940f22b6e7e513550
Camilla Björkgren <camilla_bjorkgren@hotmail.com>
2024-02-02 19:42:59 +0100

    CustomerListViewmodel

--- Catalog_App/Mvvm/ViewModels/CustomerListViewModel.cs
+++ Catalog_App/Mvvm/ViewModels/CustomerListViewModel.cs
@@ -10,6 +10,8 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace Catalog_App.Mvvm.ViewModels;
 
@@ -20,7 +22,7 @@ public partial class CustomerListViewModel : ObservableObject
     private readonly IServiceProvider _sp;
 
     [ObservableProperty]
-    private ObservableCollection<ICustomerDto> _customerList = [];
+    public ObservableCollection<ICustomerDto> _customerList = [];
 
     //[ObservableProperty]
     //private ICustomerModel _customer = new CustomerModel();
@@ -34,19 +36,20 @@ public partial class CustomerListViewModel : ObservableObject
 
         Task.Run(async () =>
         {
-            var result = _customerService.GetAllCustomersAsync();
+            var result = await _customerService.GetAllCustomersAsync();
             if (result != null)
             {
-                //CustomerList = new ObservableCollection<ICustomerDto>(result);
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    CustomerList = new ObservableCollection<ICustomerDto>(result);
+                });
             }
+           
+            
         });
 
 
-        //var result = _contactService.GetAll();
-        //if (result != null)
-        //{
-        //    ContactList = new ObservableCollection<IContact>(result);
-        //}
+       
 
         //_customer = _customerService.CurrentContact;
 
