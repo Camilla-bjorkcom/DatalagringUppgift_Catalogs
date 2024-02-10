319a3cc122fdcce79fcded6e6efcbefb8f1790bb
Camilla Björkgren <camilla_bjorkgren@hotmail.com>
2024-02-10 17:39:30 +0100

    testsss

---
+++ Shared_Catalogs.Tests/Repositories/CustomerRepository_Tests.cs
@@ -0,0 +1,208 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared_Catalogs.Contexts;
+using Shared_Catalogs.Dtos;
+using Shared_Catalogs.Entities.Customers;
+using Shared_Catalogs.Entities.Products;
+using Shared_Catalogs.Repositories;
+
+namespace Shared_Catalogs.Tests.Repositories;
+
+public class CustomerRepository_Tests
+{
+    private readonly CustomerDbContext _context =
+      new(new DbContextOptionsBuilder<CustomerDbContext>()
+      .UseInMemoryDatabase($"{Guid.NewGuid()}")
+      .Options);
+
+    [Fact]
+    public void CreateShould_AddOneTo_CustomerEntity_AndReturnEntity()
+    {
+        // Arrange
+        var customerRepository = new CustomersRepository(_context);
+        var contactInformationRepository = new ContactInformationRepository(_context);
+        var customerProfileRepository = new CustomerProfileRepository(_context);
+        var customerTypeRepository = new CustomerTypeRepository(_context);
+        var customerPhoneNumberRepository = new CustomerPhoneNumbersRepository(_context);
+        var addressRepository = new AddressesRepository(_context);
+
+        var addressEntity = addressRepository.Create(new AddressesEntity
+        {
+            StreetName = "Gatunamn",
+            PostalCode = "77777",
+            City = "Stad",
+        });
+
+        var customerTypeEntity = customerTypeRepository.Create(new CustomerTypeEntity
+        {
+            CustomerType = "kundtyp"
+        });
+
+        if (customerTypeEntity != null && addressEntity != null)
+        {
+            var customerEntity = new CustomersEntity
+            {
+                AddressesId = addressEntity.Id,
+                CustomerTypeId = customerTypeEntity.Id
+            };
+
+
+            // Act
+            var result = customerRepository.Create(customerEntity);
+
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+
+
+    [Fact]
+    public void CreateShouldNot_AddOneTo_CustomerEntity_IfAlreadyExists_AndReturnNull()
+    {
+        // Arrange
+        var customerRepository = new CustomersRepository(_context);
+        var contactInformationRepository = new ContactInformationRepository(_context);
+        var customerProfileRepository = new CustomerProfileRepository(_context);
+        var customerTypeRepository = new CustomerTypeRepository(_context);
+        var customerPhoneNumberRepository = new CustomerPhoneNumbersRepository(_context);
+        var addressRepository = new AddressesRepository(_context);
+
+        var addressEntity = addressRepository.Create(new AddressesEntity
+        {
+            StreetName = "Gatunamn",
+            PostalCode = "77777",
+            City = "Stad",
+        });
+
+        var customerTypeEntity = customerTypeRepository.Create(new CustomerTypeEntity
+        {
+            CustomerType = "kundtyp"
+        });
+
+        if (customerTypeEntity != null && addressEntity != null)
+        {
+            var customerEntity = new CustomersEntity
+            {
+                AddressesId = addressEntity.Id,
+                CustomerTypeId = customerTypeEntity.Id
+            };
+            customerRepository.Create(customerEntity);
+
+            // Act
+            var result = customerRepository.Create(customerEntity);
+
+
+            // Assert
+            Assert.Null(result);
+        }
+
+    }
+
+    [Fact]
+    public void GetAllShouldGetAllRecords_ReturnIEnumerableofTypeCustomer()
+    {
+        // Arrange
+        var customerRepository = new CustomersRepository(_context);
+        var contactInformationRepository = new ContactInformationRepository(_context);
+        var customerProfileRepository = new CustomerProfileRepository(_context);
+        var customerTypeRepository = new CustomerTypeRepository(_context);
+        var customerPhoneNumberRepository = new CustomerPhoneNumbersRepository(_context);
+        var addressRepository = new AddressesRepository(_context);
+
+        var addressEntity = addressRepository.Create(new AddressesEntity
+        {
+            StreetName = "Gatunamn",
+            PostalCode = "77777",
+            City = "Stad",
+        });
+
+        var customerTypeEntity = customerTypeRepository.Create(new CustomerTypeEntity
+        {
+            CustomerType = "kundtyp"
+        });
+
+        if (customerTypeEntity != null && addressEntity != null)
+        {
+            var customerEntity = new CustomersEntity
+            {
+                AddressesId = addressEntity.Id,
+                CustomerTypeId = customerTypeEntity.Id
+            };
+            customerRepository.Create(customerEntity);
+
+            // Act
+            var result = customerRepository.GetAll();
+
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsAssignableFrom<IEnumerable<CustomersEntity>>(result);
+        }
+    }
+
+    [Fact]
+    public void GetAllShouldNotGetAllRecords_ReturnNull()
+    {
+        // Arrange
+        var customerRepository = new CustomersRepository(_context);
+
+
+        // Act
+        var result = customerRepository.GetAll();
+
+
+        // Assert
+        Assert.Null(result);
+
+    }
+
+
+    [Fact]
+    public void GetOne_ShouldGetOneCustomer_ReturnOneCustomer()
+    {
+        // Arrange
+        var customerRepository = new CustomersRepository(_context);
+        var contactInformationRepository = new ContactInformationRepository(_context);
+        var customerProfileRepository = new CustomerProfileRepository(_context);
+        var customerTypeRepository = new CustomerTypeRepository(_context);
+        var customerPhoneNumberRepository = new CustomerPhoneNumbersRepository(_context);
+        var addressRepository = new AddressesRepository(_context);
+
+        var addressEntity = addressRepository.Create(new AddressesEntity
+        {
+            StreetName = "Gatunamn",
+            PostalCode = "77777",
+            City = "Stad",
+        });
+
+        var customerTypeEntity = customerTypeRepository.Create(new CustomerTypeEntity
+        {
+            CustomerType = "kundtyp"
+        });
+
+        if (customerTypeEntity != null && addressEntity != null)
+        {
+            var customerEntity = new CustomersEntity
+            {
+                AddressesId = addressEntity.Id,
+                CustomerTypeId = customerTypeEntity.Id
+            };
+            customerRepository.Create(customerEntity);
+
+
+            // Act
+            var result = customerRepository.GetOne(x => x.Id == customerEntity.Id);
+
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(customerEntity.Id, result.Id);
+
+        }
+
+    }
+}
+
+
+
+
--- Shared_Catalogs.Tests/Services/ProductService_Tests.cs
+++ Shared_Catalogs.Tests/Services/ProductService_Tests.cs
@@ -15,7 +15,38 @@ public class ProductService_Tests
        .Options);
 
     [Fact]
-    public void CreateProductShouid_CreateNewProduct_ReturnNewProduct()
+    public void CreateProductShould_CreateNewProduct_ReturnNewProduct()
+    {
+        // Arrange
+        var productRepository = new ProductRepository(_context);
+        var categoryRepository = new CategoryRepository(_context);
+        var categoryService = new CategoryService(categoryRepository);
+        var manufacturerRepository = new ManufacturerRepository(_context);
+        var productReviewsRepository = new ProductReviewsRepository(_context);
+        var productService = new ProductService(productRepository, productReviewsRepository, manufacturerRepository, categoryService);
+
+        var createProductDto = new CreateProductDto
+        {
+            Title = "Ny",
+            Description = "Beskrivning",
+            Manufacturer = "Tillverkare",
+            Category = "Kategori"
+        };
+
+
+        // Act
+        var result = productService.CreateProduct(createProductDto);
+
+
+        // Assert
+        Assert.NotNull(result);
+
+    }
+
+
+
+    [Fact]
+    public void CreateProductShouldNot_CreateNewProductIfTitleAlreadyExists_ReturnNull()
     {
         // Arrange
         var productRepository = new ProductRepository(_context);
@@ -32,40 +63,275 @@ public class ProductService_Tests
             Manufacturer = "Tillverkare",
             Category = "Kategori"
         };
+        productService.CreateProduct(createProductDto);
 
-        if (!productRepository.Exists(x => x.Title == createProductDto.Title))
+
+        // Act
+        var newCreateProductDto = new CreateProductDto
         {
-            var categoryEntity = categoryService.CreateCategory(createProductDto.Category);
+            Title = "Ny titel",
+            Description = "Beskrivning",
+            Manufacturer = "Tillverkare",
+            Category = "Kategori"
+        };
+         var result = productService.CreateProduct(newCreateProductDto);
+
+        // Assert
+        Assert.Null(result);
+
+        
+    }
 
 
-            var manufacturerEntity = manufacturerRepository.GetOne(x => x.ManufactureName == createProductDto.Manufacturer);
-            if (manufacturerEntity == null)
-            {
-                manufacturerEntity = manufacturerRepository.Create(new Manufacturer
-                {
-                    ManufactureName = createProductDto.Manufacturer
-                });
-            }
+    [Fact]
+    public void GetProductByTitle_ShouldReturnProductByTitle()
+    {
+        // Arrange
+        var productRepository = new ProductRepository(_context);
+        var categoryRepository = new CategoryRepository(_context);
+        var categoryService = new CategoryService(categoryRepository);
+        var manufacturerRepository = new ManufacturerRepository(_context);
+        var productReviewsRepository = new ProductReviewsRepository(_context);
+        var productService = new ProductService(productRepository, productReviewsRepository, manufacturerRepository, categoryService);
+
+        var createProductDto = new CreateProductDto
+        {
+            Title = "Ny titel",
+            Description = "Beskrivning",
+            Manufacturer = "Tillverkare",
+            Category = "Kategori"
+        };
+        productService.CreateProduct(createProductDto);
 
 
+        // Act
+        var result = productService.GetProductByTitle(createProductDto.Title);
 
-            // Act
-            var productEntity = productRepository.Create(new Product
-            {
-                ArticleNumber = Guid.NewGuid().ToString(),
-                Title = createProductDto.Title,
-                Description = createProductDto.Description,
-                CategoryId = categoryEntity.Id,
-                ManufacturerId = manufacturerEntity.Id,
+        // Assert
+        Assert.NotNull(result);
 
-            });
-
-
-            // Assert
-            Assert.NotNull(productEntity);
-
-        }
 
     }
 
+    [Fact]
+    public void GetProductByTitle_ShouldNotReturnProduct_IfNotExists()
+    {
+        // Arrange
+        var productRepository = new ProductRepository(_context);
+        var categoryRepository = new CategoryRepository(_context);
+        var categoryService = new CategoryService(categoryRepository);
+        var manufacturerRepository = new ManufacturerRepository(_context);
+        var productReviewsRepository = new ProductReviewsRepository(_context);
+        var productService = new ProductService(productRepository, productReviewsRepository, manufacturerRepository, categoryService);
+
+        var createProductDto = new CreateProductDto
+        {
+            Title = "Ny titel",
+            Description = "Beskrivning",
+            Manufacturer = "Tillverkare",
+            Category = "Kategori"
+        };
+      
+
+
+        // Act
+        var result = productService.GetProductByTitle(createProductDto.Title);
+
+        // Assert
+        Assert.Null(result);
+
+
+    }
+
+    [Fact]
+    public void GetAllProducts_ShouldReturnAllProducts_IfExists()
+    {
+        // Arrange
+        var productRepository = new ProductRepository(_context);
+        var categoryRepository = new CategoryRepository(_context);
+        var categoryService = new CategoryService(categoryRepository);
+        var manufacturerRepository = new ManufacturerRepository(_context);
+        var productReviewsRepository = new ProductReviewsRepository(_context);
+        var productService = new ProductService(productRepository, productReviewsRepository, manufacturerRepository, categoryService);
+
+        var createProductDto = new CreateProductDto
+        {
+            Title = "Ny titel",
+            Description = "Beskrivning",
+            Manufacturer = "Tillverkare",
+            Category = "Kategori"
+        };
+        productService.CreateProduct(createProductDto);
+
+
+
+        // Act
+        var result = productService.GetAllProducts();
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsAssignableFrom<IEnumerable<Product>>(result);
+    }
+
+    [Fact]
+    public void GetAllProducts_ShouldNotReturnAllProducts_IfNotExists()
+    {
+        // Arrange
+        var productRepository = new ProductRepository(_context);
+        var categoryRepository = new CategoryRepository(_context);
+        var categoryService = new CategoryService(categoryRepository);
+        var manufacturerRepository = new ManufacturerRepository(_context);
+        var productReviewsRepository = new ProductReviewsRepository(_context);
+        var productService = new ProductService(productRepository, productReviewsRepository, manufacturerRepository, categoryService);
+
+        var createProductDto = new CreateProductDto
+        {
+            Title = "Ny titel",
+            Description = "Beskrivning",
+            Manufacturer = "Tillverkare",
+            Category = "Kategori"
+        };
+        
+
+        // Act
+        var result = productService.GetAllProducts();
+
+        // Assert
+        Assert.Null(result);
+        
+    }
+
+
+    [Fact]
+    public void UpdateProducts_ShouldReturnUpdatedProduct_IfExists()
+    {
+        // Arrange
+        var productRepository = new ProductRepository(_context);
+        var categoryRepository = new CategoryRepository(_context);
+        var categoryService = new CategoryService(categoryRepository);
+        var manufacturerRepository = new ManufacturerRepository(_context);
+        var productReviewsRepository = new ProductReviewsRepository(_context);
+        var productService = new ProductService(productRepository, productReviewsRepository, manufacturerRepository, categoryService);
+
+        var createProductDto = new CreateProductDto
+        {
+            Title = "Ny titel",
+            Description = "Beskrivning",
+            Manufacturer = "Tillverkare",
+            Category = "Kategori"
+        };
+        productService.CreateProduct(createProductDto);
+
+        // Act
+        var existingProduct = productRepository.GetOne(x => x.Title == createProductDto.Title);
+        if (existingProduct != null)
+        {
+            existingProduct.Title = "Nyare Titel";
+        }
+        var result = productService.UpdateProduct(existingProduct);
+
+        // Assert
+        Assert.NotNull(result);
+
+    }
+
+    [Fact]
+    public void UpdateProducts_ShouldNotReturnUpdatedProduct_IfNotExists()
+    {
+        // Arrange
+        var productRepository = new ProductRepository(_context);
+        var categoryRepository = new CategoryRepository(_context);
+        var categoryService = new CategoryService(categoryRepository);
+        var manufacturerRepository = new ManufacturerRepository(_context);
+        var productReviewsRepository = new ProductReviewsRepository(_context);
+        var productService = new ProductService(productRepository, productReviewsRepository, manufacturerRepository, categoryService);
+
+        var createProductDto = new CreateProductDto
+        {
+            Title = "Ny titel",
+            Description = "Beskrivning",
+            Manufacturer = "Tillverkare",
+            Category = "Kategori"
+        };
+        var newProduct = productService.CreateProduct(createProductDto);
+
+        // Act
+        var existingProduct = productRepository.GetOne(x => x.Title == "titel");
+        if (existingProduct != null)
+        {
+            existingProduct.Title = "Nyare Titel";
+        }
+        var result = productService.UpdateProduct(existingProduct);
+
+        // Assert
+        Assert.Null(result);
+
+    }
+
+    [Fact]
+    public void DeleteProduct_ShouldDeleteProduct_ReturnTrue()
+    {
+        // Arrange
+        var productRepository = new ProductRepository(_context);
+        var categoryRepository = new CategoryRepository(_context);
+        var categoryService = new CategoryService(categoryRepository);
+        var manufacturerRepository = new ManufacturerRepository(_context);
+        var productReviewsRepository = new ProductReviewsRepository(_context);
+        var productService = new ProductService(productRepository, productReviewsRepository, manufacturerRepository, categoryService);
+
+        var createProductDto = new CreateProductDto
+        {
+            Title = "Ny titel",
+            Description = "Beskrivning",
+            Manufacturer = "Tillverkare",
+            Category = "Kategori"
+        };
+        var newProduct = productService.CreateProduct(createProductDto);
+
+        // Act
+        var result = productService.DeleteProduct(newProduct);
+
+        // Assert
+        Assert.True(result);
+
+    }
+
+    [Fact]
+    public void DeleteProduct_ShouldNotDeleteProduct_IfNotExists_ReturnFalse()
+    {
+        // Arrange
+        var productRepository = new ProductRepository(_context);
+        var categoryRepository = new CategoryRepository(_context);
+        var categoryService = new CategoryService(categoryRepository);
+        var manufacturerRepository = new ManufacturerRepository(_context);
+        var productReviewsRepository = new ProductReviewsRepository(_context);
+        var productService = new ProductService(productRepository, productReviewsRepository, manufacturerRepository, categoryService);
+
+        var product = new Product
+        {
+            ArticleNumber = "1",
+            Title = "Title",
+
+        };
+
+        // Act
+        var result = productService.DeleteProduct(product);
+
+        // Assert
+        Assert.False(result);
+
+    }
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
